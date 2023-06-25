c4f8d8c99216720e718f91dd167d1806d0eeb454
Michael Kutz <michael.kutz@rewe-digital.com>
2023-06-25 16:32:45 +0200

    Add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+  base
+  id("org.sonarqube") version "4.2.1.3168"
+  id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
+  id("info.solidsoft.pitest.aggregator")
+}
+
+repositories { mavenCentral() }
+
+sonar {
+  properties {
+    property("sonar.projectKey", "mkutz_stubit")
+    property("sonar.organization", "mkutz")
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
+}
+
+nexusPublishing {
+  repositories {
+    sonatype {
+      nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+      snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+    }
+  }
+}
