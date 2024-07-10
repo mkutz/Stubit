e4ac54e368208b033d1454529581db0de49b7792
Michael Kutz <michael.kutz@rewe-group.com>
2024-07-10 15:47:19 +0200

    Improve test coverage

--- modules/random/src/main/java/org/stubit/random/RandomString.java
+++ modules/random/src/main/java/org/stubit/random/RandomString.java
@@ -1,17 +1,14 @@
 package org.stubit.random;
 
+import static org.stubit.random.RandomChoice.anyOf;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /** Generates random strings. */
 public class RandomString {
 
   private RandomString() {}
-
-  /**
-   * @param character the character the string should start with
-   * @return a new {@link Builder}
-   */
-  public static Builder aStringStartingWith(char character) {
-    return new Builder(character);
-  }
 
   /**
    * @param string the character the string should start with
@@ -21,6 +18,71 @@ public class RandomString {
     return new Builder(string);
   }
 
+  /**
+   * @return a randomly selected letter from {@link Alphabet#BASIC_LATIN}.
+   */
+  public static String aLatinLetter() {
+    return aLetterFrom(Alphabet.BASIC_LATIN);
+  }
+
+  /**
+   * @param number the number of letters to generate
+   * @return a number of letters from {@link Alphabet#BASIC_LATIN}.
+   */
+  public static String latinLetters(int number) {
+    return lettersFrom(number, Alphabet.BASIC_LATIN);
+  }
+
+  /**
+   * @return a randomly selected digit from {@link DigitSystem#ARABIC}.
+   */
+  public static String anArabicDigit() {
+    return aDigitFrom(DigitSystem.ARABIC);
+  }
+
+  /**
+   * @param number the number of digits to generate
+   * @return a number of digits from {@link DigitSystem#ARABIC}.
+   */
+  public static String arabicDigits(int number) {
+    return digitsFrom(number, DigitSystem.ARABIC);
+  }
+
+  /**
+   * @param alphabet the {@link Alphabet} to select a character from
+   * @return a randomly selected character from the provided {@link Alphabet}.
+   */
+  public static String aLetterFrom(Alphabet alphabet) {
+    return anyOf(alphabet.letters()).toString();
+  }
+
+  /**
+   * @param alphabet the {@link Alphabet} to select a character from
+   * @return a randomly selected character from the provided {@link Alphabet}.
+   */
+  public static String lettersFrom(int number, Alphabet alphabet) {
+    return Stream.generate(() -> aLetterFrom(alphabet)).limit(number).collect(Collectors.joining());
+  }
+
+  /**
+   * @param digitSystem the {@link DigitSystem} to select a character from
+   * @return a randomly selected character from the provided {@link DigitSystem}.
+   */
+  public static String aDigitFrom(DigitSystem digitSystem) {
+    return anyOf(digitSystem.digits()).toString();
+  }
+
+  /**
+   * @param number the number of digits to generate
+   * @param digitSystem the {@link DigitSystem} to select the digits from
+   * @return a number of randomly selected digits from the provided {@link DigitSystem}.
+   */
+  public static String digitsFrom(int number, DigitSystem digitSystem) {
+    return Stream.generate(() -> aDigitFrom(digitSystem))
+        .limit(number)
+        .collect(Collectors.joining());
+  }
+
   /** Builds a random string. */
   public static class Builder {
 
@@ -28,19 +90,6 @@ public class RandomString {
 
     private Builder(String startString) {
       stringBuilder = new StringBuilder(startString);
-    }
-
-    private Builder(char startCharacter) {
-      stringBuilder = new StringBuilder().append(startCharacter);
-    }
-
-    /**
-     * @param character the character to append
-     * @return this
-     */
-    public Builder followedBy(Character character) {
-      stringBuilder.append(character);
-      return this;
     }
 
     /**
