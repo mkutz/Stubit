6ff26fabc062b5158dac1b7db7097bfce55a5405
Michael Kutz <michael.kutz@rewe-group.com>
2024-08-19 16:17:13 +0200

    Add RandomDurationBuilde

--- modules/random/src/main/java/org/stubit/random/RandomDuration.java
+++ modules/random/src/main/java/org/stubit/random/RandomDuration.java
@@ -1,13 +1,80 @@
 package org.stubit.random;
 
+import java.security.SecureRandom;
 import java.time.Duration;
 
+/** Generates random {@link Duration}s. */
 public class RandomDuration {
 
   private RandomDuration() {}
 
+  /**
+   * @param minInclusive the minimum value (inclusive)
+   * @param maxInclusive the maximum value (inclusive)
+   * @return a random {@link Duration} between {@code minInclusive} and {@code maxInclusive}
+   */
   public static Duration aDurationBetween(Duration minInclusive, Duration maxInclusive) {
     return Duration.ofSeconds(
         RandomLong.aLongBetween(minInclusive.toSeconds(), maxInclusive.toSeconds()));
+  }
+
+  /**
+   * @return a {@link Duration} between {@link Duration#ZERO} and {@link Long#MAX_VALUE} seconds
+   */
+  public static RandomDurationBuilder aDuration() {
+    return new RandomDurationBuilder(Duration.ZERO, Duration.ofSeconds(Long.MAX_VALUE));
+  }
+
+  /** Builds a random {@link Duration} within a specified range. */
+  public static class RandomDurationBuilder {
+
+    private final SecureRandom secureRandom = new SecureRandom();
+    private Duration minInclusive;
+    private Duration maxInclusive;
+
+    private RandomDurationBuilder(Duration min, Duration max) {
+      this.minInclusive = min;
+      this.maxInclusive = max;
+    }
+
+    /**
+     * @param minInclusive the minimum value (inclusive)
+     * @return this
+     * @throws IllegalArgumentException if {@code minInclusive} is greater than or equal to {@link
+     *     #maxInclusive}
+     */
+    public RandomDurationBuilder min(Duration minInclusive) {
+      this.minInclusive = minInclusive;
+      if (minInclusive.compareTo(maxInclusive) > 0) {
+        throw new IllegalArgumentException(
+            "Can't set min to %s, as it must not be greater than max (%s)"
+                .formatted(minInclusive, maxInclusive));
+      }
+      return this;
+    }
+
+    /**
+     * @param maxInclusive the maximum value (inclusive)
+     * @return this
+     * @throws IllegalArgumentException if {@code maxInclusive} is less than or equal to {@link
+     *     #minInclusive}
+     */
+    public RandomDurationBuilder max(Duration maxInclusive) {
+      if (minInclusive.compareTo(maxInclusive) > 0) {
+        throw new IllegalArgumentException(
+            "Can't set max to %s, as it must not be less than min (%s)"
+                .formatted(maxInclusive, minInclusive));
+      }
+      this.maxInclusive = maxInclusive;
+      return this;
+    }
+
+    /**
+     * @return a random {@link Duration} between {@link #minInclusive} and {@link #maxInclusive}
+     */
+    public Duration build() {
+      return Duration.ofSeconds(
+          secureRandom.nextLong(minInclusive.toSeconds(), maxInclusive.toSeconds()));
+    }
   }
 }
